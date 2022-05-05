135405d173c37b2345f89f582167da620cc5bea9
danya090699 <danya090699@yandex.ru>
2022-05-06 00:32:38 +0400

    fix registration

--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,21 +1,18 @@
 <template>
     <Shell ref="shell" :data="[students]" class="pa-2">
         <template v-slot:default>
-            <template v-if="students.students.length">
-                <center class="font-weight-bold my-2">Зарегистрировавшиеся ученики</center>
-                <v-simple-table>
-                    <thead>
-                    <tr>
-                        <th>Имя</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="student in students.students">
-                        <td @click="show_student(student.id)">{{ student.name }}</td>
-                    </tr>
-                    </tbody>
-                </v-simple-table>
-            </template>
+            <v-simple-table v-show="students.length">
+                <thead>
+                <tr>
+                    <th>Имя</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="student in students">
+                    <td @click="show_student(student.id)">{{ student.name }}</td>
+                </tr>
+                </tbody>
+            </v-simple-table>
 
             <div class="d-flex align-center">
                 <v-btn @click="create_user(student_name, true)" class="mr-2" color="info" small>создать ученика</v-btn>
@@ -25,28 +22,6 @@
                 <v-btn @click="create_user(teacher_name, false)" class="mr-2" color="info" small>создать учителя</v-btn>
                 <v-text-field v-model="teacher_name" placeholder="Имя учителя"/>
             </div>
-
-            <template v-if="students.not_registered.length">
-                <center class="font-weight-bold my-2">Незарегистрировавшиеся пользователи</center>
-                <v-simple-table>
-                    <thead>
-                    <tr>
-                        <th>Имя</th>
-                        <th>Тип</th>
-                        <th>Ссылка для регистрации</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="user in students.not_registered">
-                        <td>{{ user.name }}</td>
-                        <td>{{ user.is_student ? "ученик" : "учитель" }}</td>
-                        <td>
-                            <v-btn @click="to_clipboard(user.url)" small outlined color="grey">скопировать</v-btn>
-                        </td>
-                    </tr>
-                    </tbody>
-                </v-simple-table>
-            </template>
         </template>
     </Shell>
 </template>
@@ -61,29 +36,22 @@ export default {
     }),
     computed: {
         students() {
-            console.log(this.store.get(["get_students", {}]))
             return this.store.get(["get_students", {}])
         }
     },
     methods: {
         create_user(name, is_student) {
             this.$refs.shell.query({
-                confirm: `Создать ${is_student ? "ученика" : "учителя"} ${name}?`,
                 toServer: ["create_user", {name, is_student}],
-                waiting: `Создание ${is_student ? "ученика" : "учителя"} ${name}`,
-                accepted: (data) => {
-                    console.log(data)
-                    this.store.load(["get_students", {}])
-                    return `${is_student ? "Ученик" : "Учитель"} ${name} создан`;
+                waiting: `Создание ссылки для ${is_student ? "ученика" : "учителя"} ${name}`,
+                accepted: (url) => {
+                    navigator.clipboard.writeText(url);
+                    return `Ссылка скопирована в буфер`;
                 },
                 rejected: (message) => {
-                    console.log(message)
-                    return `Произошла ошибка при создании ${is_student ? "ученика" : "учителя"} ${name}: ${message}`;
+                    return `Произошла ошибка при создании ссылки для ${is_student ? "ученика" : "учителя"} ${name}: ${message}`;
                 }
             })
-        },
-        to_clipboard(text) {
-            navigator.clipboard.writeText(text);
         },
         show_student(id) {
             this.addScreen({type: "StudentTasks", info: {student_id: id}});
